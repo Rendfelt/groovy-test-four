46b9829068c4964f4535a8ba2569769051df2af1
teslin <viktorteslin@gmail.com>
2019-08-05 16:05:43 +0300

    initial commit

---
+++ src/main/java/org/dragard/groovy/test/App.java
@@ -0,0 +1,13 @@
+package org.dragard.groovy.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
